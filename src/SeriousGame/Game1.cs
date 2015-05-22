b4c6735c13d46280bdc6695e2a6aeed4dce0f5b8
WensinkDesign <ianwensink@Ians-MacBook-Pro.local>
2015-05-22 12:46:36 +0200

    Mac support

--- src/SeriousGame/Game1.cs
+++ src/SeriousGame/Game1.cs
@@ -60,9 +60,7 @@ namespace SeriousGame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
+          
             // TODO: Add your update logic here
 
             base.Update(gameTime);
