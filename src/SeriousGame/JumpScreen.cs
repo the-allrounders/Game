bd57f84b6b51d90cc53cb6ea3103a25f09e21670
ianwensink <ianwensink@gmail.com>
2015-06-01 14:56:13 +0100

    Revert "Updated Platform.cs and JumpScreen.cs for platforms"
    This reverts commit 02e92a9ef9cee68d6dad03141896a759216050e4.

--- src/SeriousGame/JumpScreen.cs
+++ src/SeriousGame/JumpScreen.cs
@@ -10,13 +10,12 @@ namespace SeriousGame
     class JumpScreen : GameScreen
     {
         int offset = 0;
-        const int gameHeight = 10000;
 		private List<Platform> _platforms = new List<Platform>();
         
         public override void Load()
         {
             // START THE JUMPING
-			addPlatforms ();
+			addPlatform (new Vector2 (100, offset + 100), new Vector2 (10, 100));
         }
 
         public override void Unload()
@@ -24,49 +23,19 @@ namespace SeriousGame
             // END THE JUMPING
         }
 
-        public float calculateDistance ()
-        {
-            float _distance;
-            Console.WriteLine(_platforms.Count);
-            if (_platforms.Count > 0)
-            {
-                Rectangle highestPlatform = _platforms[_platforms.Count - 1].boundingBox;
-                if (highestPlatform.Left > ScreenManager.Instance.Dimensions.X / 2)
-                {
-                    // highestPlatform is right from center
-                    _distance = highestPlatform.Left - highestPlatform.Width - 200;
-                }
-                else
-                {
-                    // highestPlatform is left from center
-                    _distance = highestPlatform.Left + highestPlatform.Width + 200;
-                }
-            }
-            else
-            {
-                _distance = ScreenManager.Instance.Dimensions.X / 2;
-            }
-            Console.WriteLine(_distance);
-            return _distance;
-        }
-
-		public void addPlatforms ()
-        {
-            for (int i = 100; i < gameHeight; i++)
-            {
-                _platforms.Add(new Platform(new Vector2(calculateDistance(), -50), new Vector2(150, 50));
-            }
+		public void addPlatform (Vector2 pos, Vector2 size) {
+			_platforms.Add (new Platform (pos, size));
+			Console.WriteLine ("add platform!");
 		}
 
         public override void Update(GameTime gameTime)
         {
             offset += 1;
 			foreach (var platform in _platforms) {
-                Console.WriteLine(platform.boundingBox.Top);
 				if (platform.boundingBox.Top > ScreenManager.Instance.Dimensions.Y) {
 					_platforms.RemoveAt(_platforms.IndexOf (platform));
-                    addPlatform(new Vector2(calculateDistance(), offset + -50), new Vector2(50, 100));
-                } 
+					addPlatform (new Vector2 (offset, 0), new Vector2 (200, 50));
+				} 
 				else {
 					platform.Update (gameTime, offset);
 				}
