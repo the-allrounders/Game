07fa4a9af5bae1ea51e6736e5e35022153316542
nylsoo <tnnielstn@gmail.com>
2015-06-02 14:10:16 +0200

    FontManager

---
+++ src/SeriousGame/Managers/FontManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeriousGame
+{
+    class FontManager
+    {
+        private static FontManager _instance;
+        public static FontManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new FontManager();
+                }
+                return _instance;
+            }
+        }
+
+
+        public SpriteFont Verdana;
+        public SpriteFont VerdanaBold;
+        public SpriteFont VerdanaRegular;
+
+
+        public void Load(ContentManager Content)
+        {
+            this.Verdana = Content.Load<SpriteFont>("Verdana");
+            this.VerdanaBold = Content.Load<SpriteFont>("Verdana35Bold");
+            this.VerdanaRegular = Content.Load<SpriteFont>("Verdana35Regular");
+        }
+    }
+}
