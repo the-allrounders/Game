3709d92f720414dd53ae879c5c5fc5e4af0a16eb
ianwensink <ianwensink@gmail.com>
2015-06-08 16:43:57 +0200

    Fix stuck at leaderboard
    Now you can return to the startscreen

--- src/SeriousGame/Screens/LeaderboardScreen.cs
+++ src/SeriousGame/Screens/LeaderboardScreen.cs
@@ -23,7 +23,11 @@ namespace SeriousGame
 
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
-            
+            // If user is pressing ESC, return to StartScreen
+            if (InputManager.IsPressing(Keys.Escape) || InputManager.IsClicking(new Rectangle((int)ScreenManager.Dimensions.X / 2 - 35, (int)ScreenManager.Dimensions.Y / 2 + 200, 100, 20)))
+            {
+                ScreenManager.CurrentScreen = new StartScreen();
+            }
         }
 
         public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
@@ -34,6 +38,7 @@ namespace SeriousGame
                 String text = (i + 1) + ". " + score[0] + ": " + score[1];
                 spriteBatch.DrawString(FontManager.Verdana, text, new Vector2(ScreenManager.Dimensions.X / 2 - 150, 100 + (i * 40)), Color.Black);
             }
+            spriteBatch.DrawString(FontManager.Verdana, "Terug", new Vector2(ScreenManager.Dimensions.X / 2 - 35, ScreenManager.Dimensions.Y / 2 + 200), Color.Black);
         }
     }
 }
