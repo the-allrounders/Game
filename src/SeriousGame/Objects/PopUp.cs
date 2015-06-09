d3c0c9065b0c583b18481482f98992c7192a27f9
lisauijtewaal <Lisa@MBP-van-Lisa.lan>
2015-06-09 23:23:07 +0200

    Update questions

--- src/SeriousGame/Objects/PopUp.cs
+++ src/SeriousGame/Objects/PopUp.cs
@@ -8,20 +8,41 @@ namespace SeriousGame.Objects
     class PopUp
     {
         private readonly string[,] questions = {
-                                                    {"Hoe heet het heete rode spul dat van onderen komt?", 
-                                                    "1) Mugma","2) Mogma", "3) Magma", "4) Ian"}, 
-                                                    {"Hoe heet de vader van Bart", 
-                                                    "1) Peter","2) Henk", "3) Harry", "4) Bart"},   
-                                                    {"het is groen en het kan vliegen", 
-                                                    "1) Appels","2) Voetballen", "3) Wortels", "4) Groene vogels"},   
-                                                    {"Zijn bananen krom?", 
-                                                    "1) Ja","2) Nee", "3) Misschien", "4) Als je elleboog op tafel doet misschien"}   
+			{"Hoe noemt men vloeibaar gesteente dat zich onder het aardoppervlak bevindt?", 
+				"1) Lava","2) Karma", "3) Magma", "4) Vulva"},
+			{"Hoe wordt de wolk genoemd die te zien is tijdens een vulkaanuitbarsting?", 
+				"1) Vuurwolk", "2) Rookwolk", "3) Aswolk"}
+			{"Hoe noemt men een vulkaan dat op het moment niet actief is maar wel terug actief kan worden?", 
+                "1) Slapende vulkaan","2) Inactieve vulkaan", "3) Rustende vulkaan", "4) Snurkende vulkaan"}, 
+			{"Hoe worden vulkanen genoemd die zich onder het wateroppervlak bevinden?", 
+                "1) Stratovulkanen","2) Oceanische vulkanen", "3) Schildvulkanen", "4) Submarine vulkanen"},   
+			{"Wat is de grootste actieve vulkaan in Europa?", 
+               "1) Vesuvius","2) Stromboli", "3) Etna", "4) Vulcano"},   
+			{"Hoe noemt men een vulkaan die is uitgedooft?", 
+				"1) Arme vulkaan","2) Dode vulkaan", "3) Levensloze vulkaan"},  
+			{"Stelling: Afgekoelde lava is vruchtbaar", 
+				"1) Onjuist", "2) juist"},  
+			{"Welk land heeft de meeste vulkanen?", 
+				"1) Italië", "2) Indonesië", "3) Nieuw-Zeeland"},
+			{"Hoe heet het gat aan de top van een vulkaan?", 
+				"1) De krater", "2) De kraterpijp", "3) De mond"},
+			{"Wat is geen soort vulkaan?", 
+				"1) Spleetvulkaan","2) Caldeira","3) Pacificvulkaan ", "4) Stratovulkaan", "5) Schildvulkaan"},
+			
                                             };
         private readonly int[,] answers = {
-                                                {3},
-                                                {2},
+												{3},
+												{3},
+                                                {1},
                                                 {4},
-                                                {1}
+                                                {3},
+												{2},
+												{2},
+												{2},
+												{1},
+												{3}
+
+                                                
         };
         private readonly int questionNumber;
         private readonly string choices;
