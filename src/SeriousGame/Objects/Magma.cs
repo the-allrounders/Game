a48077dd5fe4441ffadbe0bdac1308b9040b6caf
ianwensink <ianwensink@gmail.com>
2015-06-09 21:36:44 +0200

    Clean up Magma.cs

--- src/SeriousGame/Objects/Magma.cs
+++ src/SeriousGame/Objects/Magma.cs
@@ -38,24 +38,8 @@ namespace SeriousGame.Objects
         public void Draw(SpriteBatch spriteBatch, int offset)
         {
             int bottomScreen = offset * -1 + (int)ScreenManager.Dimensions.Y;
-            decimal iterations;
             int magmaHeight = ((int)magmaPosition.Y - bottomScreen) * -1;
-            if (magmaPosition.Y < bottomScreen)
-            {
-                if (magmaHeight > ScreenManager.Dimensions.Y)
-                    iterations = (int)ScreenManager.Dimensions.Y / magmaTexture.Height + 6;
-                else
-                    iterations = magmaHeight / magmaTexture.Height + 1;
-            }
-            else
-            {
-                iterations = 0;
-            }
-
-            for (int i = 0; i < Math.Ceiling(iterations); i++)
-            {
-                spriteBatch.Draw(magmaTexture, new Vector2(JumpScreen.Padding-13, bottomScreen + offset - magmaHeight + magmaTexture.Height * i));
-            }
+            spriteBatch.Draw(magmaTexture, new Vector2(JumpScreen.Padding-13, bottomScreen + offset - magmaHeight));
         }
     }
 }
